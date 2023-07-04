8c86b02d478d41c8cbdfef082a6923a92a8169b4
Din Ho <hodin81@gmail.com>
2023-07-03 18:31:58 -0700

    Updated resume and Projects description

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,10 +3,9 @@ import acr from '../../assets/acr-768.avif';
 import photography from '../../assets/photography-768.avif';
 import portfolio from '../../assets/portfolio-768w.avif';
 import timeattask from '../../assets/time-attask.avif';
-import { FaCss3Alt, FaReact, FaSass, FaWordpress, FaExternalLinkAlt } from 'react-icons/fa';
-import { TbBrandJavascript } from 'react-icons/tb';
-import { SiVite, SiNetlify } from 'react-icons/si';
-import { TiHtml5 } from 'react-icons/ti';
+import ageCalculator from '../../assets/age-calculator.avif';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+
 
 const Projects = () => {
     return(
@@ -18,6 +17,27 @@ const Projects = () => {
             <p className='projects-subtitle'>My recent and notable projects.</p>
             <div className='projects-flex-container'>
                 <figure className='projects-box'>
+                    <a href="https://age-calculator-react-tailwindcss.netlify.app/" target='_blank'>
+                        <img src={ageCalculator} alt="age-calculator" className='image' width='768' height='438'/>
+                    </a>
+                </figure>
+                <div className='projects-description'>
+                    <h3 className='projects-name'>Age Calculator</h3>
+                    <p>A web application that will calculate and return the exact age of a user's birthday in years, months, and days. This web app is responsive and accessible on mobile devices. Google Lighthouse audit score: <strong>100 Performance, 100 Accessibility, 100 Best Practices, 100 SEO.</strong></p>
+                    <span>Made with:</span>
+                    <ul>
+                        <li> HTML</li>
+                        <li> Tailwind CSS</li>
+                        <li> JavaScript</li>
+                        <li> Vite</li>
+                        <li> React.js</li>
+                        <li> Netlify</li>
+                    </ul>
+                    <p className='website-button-hover'>
+                        <a href="https://age-calculator-react-tailwindcss.netlify.app/" target='_blank' className='website-button'>Website <FaExternalLinkAlt /></a>       
+                    </p>
+                </div>
+                <figure className='projects-box'>
                     <a href="https://timeattask.netlify.app/" target='_blank'>
                         <img src={timeattask} alt="time-atttask" className='image' width='768' height='438'/>
                     </a>
@@ -27,13 +47,13 @@ const Projects = () => {
                     <p>A customizable pomodoro timer to  increase productivity. The goal is to help users identify, prioritize, and complete their daily tasks in time intervals with breaks. This web app is responsive and accessible on mobile devices. Google Lighthouse audit score: <strong>100 Performance, 100 Accessibility, 100 Best Practices, 100 SEO.</strong></p>
                     <span>Made with:</span>
                     <ul>
-                        <li><TiHtml5 className='techs'/> <br /> HTML</li>
-                        <li><FaCss3Alt className='techs'/> <br /> CSS</li>
-                        <li><TbBrandJavascript className='techs'/> <br /> JavaScript</li>
-                        <li><SiVite className='techs'/> <br /> Vite</li>
-                        <li><FaReact className='skills'/> <br />React.js</li>
-                        <li><FaSass className='skills'/> <br />Sass</li>
-                        <li><SiNetlify className='skills'/> <br />Netlify</li>
+                        <li> HTML</li>
+                        <li> CSS</li>
+                        <li> JavaScript</li>
+                        <li> Vite</li>
+                        <li> React.js</li>
+                        <li> Sass</li>
+                        <li> Netlify</li>
                     </ul>
                     <p className='website-button-hover'>
                         <a href="https://timeattask.netlify.app/" target='_blank' className='website-button'>Website <FaExternalLinkAlt /></a>       
@@ -49,13 +69,13 @@ const Projects = () => {
                     <p>A multi-page photographer portfolio website where users can browse and view photo collections through a pop-up carousel image slider. The website is responsive and accessible on mobile devices.</p>
                     <span>Made with:</span>
                     <ul>
-                        <li><TiHtml5 className='techs'/> <br /> HTML</li>
-                        <li><FaCss3Alt className='techs'/> <br /> CSS</li>
-                        <li><TbBrandJavascript className='techs'/> <br /> JavaScript</li>
-                        <li><SiVite className='techs'/> <br /> Vite</li>
-                        <li><FaReact className='skills'/> <br />React.js</li>
-                        <li><FaSass className='skills'/> <br />Sass</li>
-                        <li><SiNetlify className='skills'/> <br />Netlify</li>
+                        <li> HTML</li>
+                        <li> CSS</li>
+                        <li> JavaScript</li>
+                        <li> Vite</li>
+                        <li> React.js</li>
+                        <li> Sass</li>
+                        <li> Netlify</li>
                     </ul>
                     <p className='website-button-hover'>
                         <a href="https://dinvhophotography.netlify.app/home" target='_blank' className='website-button'>Website <FaExternalLinkAlt /></a>       
@@ -71,9 +91,9 @@ const Projects = () => {
                     <p> Designed, built, and launched a modern and interactive multi-page website for the clients with WordPress and custom HTML/CSS. The website is responsive and accessible on mobile devices.</p>
                     <span>Made with:</span>
                     <ul>
-                        <li><TiHtml5 className='techs'/> <br /> HTML</li>
-                        <li><FaCss3Alt className='techs'/> <br /> CSS</li>
-                        <li><FaWordpress className='techs'/> <br />WordPress</li>
+                        <li> HTML</li>
+                        <li> CSS</li>
+                        <li> WordPress</li>
                     </ul>
                     <p className='website-button-hover'>
                         <a href="https://acrglazing.com/" target='_blank' className='website-button'>Website <FaExternalLinkAlt /></a>       
@@ -89,13 +109,13 @@ const Projects = () => {
                     <p>A 100 points portfolio website including a working contact form submission hosted on Netlify. The website is responsive and accessible on mobile devices. Google Lighthouse audit score: <strong>100 Performance, 100 Accessibility, 100 Best Practices, 100 SEO.</strong></p>
                     <span>Made with:</span>
                     <ul>
-                        <li><TiHtml5 className='techs'/> <br /> HTML</li>
-                        <li><FaCss3Alt className='techs'/> <br /> CSS</li>
-                        <li><TbBrandJavascript className='techs'/> <br /> JavaScript</li>
-                        <li><SiVite className='techs'/> <br /> Vite</li>
-                        <li><FaReact className='skills'/> <br />React.js</li>
-                        <li><FaSass className='skills'/> <br />Sass</li>
-                        <li><SiNetlify className='skills'/> <br />Netlify</li>
+                        <li> HTML</li>
+                        <li> CSS</li>
+                        <li> JavaScript</li>
+                        <li> Vite</li>
+                        <li> React.js</li>
+                        <li> Sass</li>
+                        <li> Netlify</li>
                     </ul>
                     <p className='website-button-hover'>
                         <a href="https://github.com/hodinsay/portfolio-website-template" target='_blank' className='website-button'>GitHub <FaExternalLinkAlt /></a>       
